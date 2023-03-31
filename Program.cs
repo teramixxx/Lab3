aa1ffa9c5e69b97b72a82d9132de38b0f5e5ccc9
teramixxx <127118011+teramixxx@users.noreply.github.com>
2023-03-31 10:44:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    public interface ITranslationStrategy
+    {
+        string Translate(string phrase);
+    }
+
+    public class AmericanTranslationStrategy : ITranslationStrategy
+    {
+
+        public string Translate(string phrase)
+        {
+            return phrase + ", Степан!";
+        }
+    }
+
+    public class AustralianTranslationStrategy : ITranslationStrategy
+    {
+
+        public string Translate(string phrase)
+        {
+            return phrase + ", Сергiй!";
+        }
+    }
+
+    public class EnglishTranslation
+    {
+        public static string Translate(string phrase,
+                ITranslationStrategy strategy)
+        {
+            return strategy.Translate(phrase);
+        }
+
+        static void Main(string[] args)
+        {
+            string aussieHello = Translate("Привiт",
+                    new AustralianTranslationStrategy());
+            Console.WriteLine(aussieHello);
+            string usaHello = Translate("Привiт",
+                    new AmericanTranslationStrategy());
+            Console.WriteLine(usaHello);
+            Console.ReadLine();
+        }
+    }
+
+}
